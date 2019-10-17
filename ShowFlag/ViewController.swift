45a63a9c140d7120bd4caf10c880e9e4c8fce610
Alexander Lebedev <alexander.s.lebedev@gmail.com>
2019-10-17 12:28:59 +0700

    Initial Commit

---
+++ ShowFlag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowFlag
+//
+//  Created by alexander on 17.10.2019.
+//  Copyright © 2019 alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
